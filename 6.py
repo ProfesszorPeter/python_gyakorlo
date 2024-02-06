f4954582a6d68ac3ece5f2432b82d97f86f9c2dc
ProfesszorPeter <simonpetymeg@gmail.com>
2024-02-06 06:17:43 +0100

    8. feladat

--- 6.py
+++ 6.py
@@ -1,3 +1,8 @@
 a = int(input("Adja meg az 1. egész számot: "))
 b = int(input("Adja meg az 2. egész számot: "))
 c = int(input("Adja meg az 3. egész számot: "))
+
+if a % 2 == 0 and b % 2 == 0 and c % 2 == 0:
+    print("Mind a 3 szám páros")
+else:
+    print("valamely szám nem páros")
---
+++ 8.py
@@ -0,0 +1,6 @@
+# Írj egy Python programot, amely bekér egy valós (A) és egy egész (K) számot a felhasználótól és kiírja a képernyőre az AK hatvány értékét anélkül, hogy használnád a ** operátort!
+
+A = int(input("Adjon meg egy valós számot: "))
+K = int(input("Adjom meg egy egész számot: "))
+
+
