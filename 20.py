780cc09e12482792a0cf4aff6ab721ef369775bf
ProfesszorPeter <simonpetymeg@gmail.com>
2024-02-12 11:12:45 +0100

    szöveg betűininek új sorba írása

---
+++ 20.py
@@ -0,0 +1,10 @@
+# Írj egy Python programot, amely bekér egy szót (sztringet) a felhasználótól
+# és kiírja a képernyőre a szó betűit, úgy, hogy minden betű egy új sorba kerüljön!
+
+
+a = input("adjon meg egy szót: ")
+
+b = []
+
+for i in a:
+    print(i)
