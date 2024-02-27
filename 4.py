57065f27a21ddc93ab75e22cffe1175b186d19f7
ProfesszorPeter <simonpetymeg@gmail.com>
2024-02-27 15:00:20 +0100

    javítás

--- 4.py
+++ 4.py
@@ -1,7 +1,6 @@
 a = int(input("adjon megy egy egész számot: "))
 
 if a // 3:
-    print(f"{a} nem osztható 3-mal")
-
-if a // 5:
+    print(f"{a} osztható 3-mal")
+elif a // 5:
     print(f"{a} osztható 5-el")
